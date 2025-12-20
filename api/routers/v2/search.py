4799bbe4756f91b3098a4f77d6d48c6c301e87bb
Markbeep <mrkswrn@gmail.com>
2025-12-20 15:25:55 +0000

    fix(api): translate fs/hs to correct semester

--- api/routers/v2/search.py
+++ api/routers/v2/search.py
@@ -182,9 +182,19 @@ def match_filters(
                 case Operator.le:
                     query = query.where(year <= year_value)
         elif filter_.key == "semester":
-            if len(filter_.value) == 0 or filter_.value[0].lower() not in ["s", "w"]:
+            if len(filter_.value) == 0 or filter_.value[0].upper() not in [
+                "S",
+                "W",
+                "F",
+                "H",
+            ]:
                 continue
-            query = query.where(semester == filter_.value[0].upper())
+            semester = filter_.value[0].lower()
+            if semester == "F":  # fs
+                semester = "S"
+            elif semester == "H":  # hs
+                semester = "W"
+            query = query.where(semester == semester.upper())
         elif filter_.key in ["lecturer", "i", "instructor"]:
             query = query.where(
                 or_(
